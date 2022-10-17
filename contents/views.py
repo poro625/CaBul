ecb92a2864a008096088bcefea2445edc09a5713
hanmariyang <hanmari.code@gmail.com>
2022-10-17 20:25:53 +0900

    [업데이트] upload.html, contents/views.py (피드 업로드 기능 추가)
    1)upload.html : 기본 기능 구성 (카테고리, 태그, 내용 작성)
    2) views.py : Upload 기능 업데이트 ( user(FK), content, like, category, tags, image)

--- contents/views.py
+++ contents/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from users.models import User
 
 # Create your views here.
+
+def Upload(request):
+    if request.method == 'GET':  # 요청하는 방식이 GET 방식인지 확인하기
+        return render(request, 'upload.html')
+
+    if request.method == 'POST':
+        my_feed = Feed()  # 글쓰기 모델 가져오기
+        my_feed.user = models.ForeignKey('users.User', on_delete = models.CASCADE)  # 모델에 사용자 저장
+        my_feed.content = request.POST.get('content', '')  # 모델에 글 저장
+        my_feed.like = 0
+        my_feed.image = "https://i1.ruliweb.com/img/22/10/04/1839e60028750ad5d.jpg"
+        my_feed.category = request.POST.get('category', '') # 모델에 카테고리 저장
+        my_feed.tags = request.POST.get('tag', '').split(',')
+        for tag in tags:
+            tag = tag.strip()
+            if tag != '': # 태그를 작성하지 않았을 경우에 저장하지 않기 위해서
+                my_feed.tags.add(tag)
+        my_feed.save()
+        my_feed.save()
+        return redirect('/')
