0d3913683e8ba7fa3faa05957af38f6b542b9a0d
devjunseok <wnslgood58@naver.com>
2022-10-18 00:58:48 +0900

    이미지 기능  작업중

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import *
 
 app_name='users'
 
@@ -10,5 +11,6 @@ urlpatterns = [
     path('update/<int:id>/', views.update, name='update'),
     path('password/<int:id>', views.password, name='password'),
     path('delete/', views.delete, name='delete'),
+    path('fileupload/', fileUpload, name="fileupload"),
     
 ]
