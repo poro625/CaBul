54aa41b35083938cc48618c51c5f4ba558b05219
poro625 <poro625@naver.com>
2022-10-18 22:34:42 +0900

    [생성] 좋아요 url 추가

--- contents/urls.py
+++ contents/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('<int:feed_id>/',views.detail_comment,name='detail_comment'), # 댓글 읽기
     path('comment/<int:feed_id>',views.write_comment, name='write_comment'), # 댓글 쓰기
     path('comment/delete/<int:feed_id>',views.delete_comment, name='delete_comment'), # 해당 삭제
+    path('<int:feed_pk>/likes/', views.likes, name='likes'), # 좋아요 
 ]
 
