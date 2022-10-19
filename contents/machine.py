7264769eda283f050add75400606942547aa9505
devjunseok <wnslgood58@naver.com>
2022-10-19 20:57:55 +0900

    [업데이트] - 게시글 수정 시 사진 수정 가능 추가 및 게시글 수정해도 카테고리 자동 분류 기능 추가

--- contents/machine.py
+++ contents/machine.py
@@ -16,4 +16,18 @@ def yolo(img, my_feed):
         category_name = '카테고리 없음'
         my_feed.category = category_name
         my_feed.save()
+
+def yolo2(img, post):
+    try:
+        model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
+        imgs = [(f'./media/{img}')] # batch of images
+
+        results = model(imgs)
         
+        category_name = results.pandas().xyxy[0]['name'][0]
+        post.category = category_name
+        post.save()
+    except(IndexError):
+        category_name = '카테고리 없음'
+        post.category = category_name
+        post.save()
