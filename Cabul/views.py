22d209d83372a9e2a124f4472214be475b7abd22
hanmariyang <hanmari.code@gmail.com>
2022-10-19 18:01:46 +0900

    [업데이트] base, views.home, 카테고리 출력 기능 추가
    1) views.home : home.html 카테고리 값 출력값 보내주기
    2) base.html : 카테고리 값 받아서 for문 출력
     * base.hrml 이 보여주는 함수 전부 수정 필요

--- Cabul/views.py
+++ Cabul/views.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from django.shortcuts import render, redirect
 from contents.models import Feed
 
@@ -9,7 +10,11 @@ def home(request): # home 화면
         user = request.user.is_authenticated  # 사용자가 인증을 받았는지 (로그인이 되어있는지)
         if user:
             feed = Feed.objects.all().order_by('-created_at')
-            return render(request, 'home.html', { 'feeds' : feed })
+            feed_cate = Feed.objects.all().order_by('-category')
+            feed_category = feed_cate.values_list('category', flat=True).distinct()
+
+            return render(request, 'home.html', { 'feeds' : feed, 'categorys' : feed_category })
         else:
             return redirect('users/login')
+
 
